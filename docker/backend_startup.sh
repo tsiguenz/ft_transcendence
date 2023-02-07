b80f4dd391768e11db243b0e87f6305ea04952b9
Crysicia <7459685+Crysicia@users.noreply.github.com>
2023-02-07 16:44:26 +0100

    Fixe backend and makew it multi-platform

--- docker/backend_startup.sh
+++ docker/backend_startup.sh
@@ -1,17 +1,8 @@
 #!/bin/sh
 
 set -e
-if (uname -a | grep x86_64); then
-	if [[ -f /lib/libc.musl-x86_64.so.1 ]]; then
-		mv /lib/x86_64-linux-musl/libc.so /lib/libc.musl-x86_64.so.1
-	fi
-else
-	if [[ -f /lib/libc.musl-aarch64.so.1 ]]; then
-		mv /lib/aarch64-linux-musl/libc.so /lib/libc.musl-aarch64.so.1
-	fi
-fi
 
 npm install --only=development
-npx prisma migrate dev
+npx prisma migrate dev --name init
 npm run start:dev
 rm -rf backend_startup.sh
