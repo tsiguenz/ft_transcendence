97f20970f40e15765b4dcadae305cad8cd206e47
Thibaut SIGUENZA <tsiguenz@e1r11p4.clusters.42paris.fr>
2023-02-07 16:24:38 +0100

    adapt to x86_64 but didn't work

--- docker/backend_startup.sh
+++ docker/backend_startup.sh
@@ -1,6 +1,16 @@
 #!/bin/sh
 
 set -e
+if (uname -a | grep x86_64); then
+	if [[ -f /lib/libc.musl-x86_64.so.1 ]]; then
+		mv /lib/x86_64-linux-musl/libc.so /lib/libc.musl-x86_64.so.1
+	fi
+else
+	if [[ -f /lib/libc.musl-aarch64.so.1 ]]; then
+		mv /lib/aarch64-linux-musl/libc.so /lib/libc.musl-aarch64.so.1
+	fi
+fi
+
 npm install --only=development
 npx prisma migrate dev
 npm run start:dev
