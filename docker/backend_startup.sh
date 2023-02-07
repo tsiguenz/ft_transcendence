8a773a3a17c1ab2e9cc2ecfd643971e4758a87bd
Crysicia <7459685+Crysicia@users.noreply.github.com>
2023-02-07 17:50:29 +0100

    Add ARM64 support (#1)
    * Add working dockerfile for arm64
    
    * adapt to x86_64 but didn't work
    
    * Fixe backend and makew it multi-platform
    
    * clean docker backend
    
    * Fix backend entrypoint
    
    ---------
    
    Co-authored-by: Thibaut SIGUENZA <tsiguenz@e1r11p4.clusters.42paris.fr>

--- docker/backend_startup.sh
+++ docker/backend_startup.sh
@@ -1,7 +1,12 @@
-#!/bin/sh
+#!/bin/bash
 
 set -e
+
+if [[ $(uname -a | grep aarch64) ]] && [[ ! -e /lib/libc.musl-aarch64.so.1 ]]; then
+	mv /lib/aarch64-linux-musl/libc.so /lib/libc.musl-aarch64.so.1
+fi
+
 npm install --only=development
-npx prisma migrate dev
-npm run start:dev
-rm -rf backend_startup.sh
+npx prisma migrate dev --name init
+
+exec "$@"
