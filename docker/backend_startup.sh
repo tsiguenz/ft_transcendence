4af1b068e8f21efc54862a13a11c23790de7523e
Thibaut SIGUENZA <tsiguenz@e1r11p4.clusters.42paris.fr>
2023-02-07 17:02:19 +0100

    clean docker backend

--- docker/backend_startup.sh
+++ docker/backend_startup.sh
@@ -2,7 +2,11 @@
 
 set -e
 
+if [[ ( uname -a | grep arm64 ) && -f /lib/libc.musl-aarch64.so.1 ]]; then
+	mv /lib/aarch64-linux-musl/libc.so /lib/libc.musl-aarch64.so.1
+fi
+
 npm install --only=development
 npx prisma migrate dev --name init
-npm run start:dev
-rm -rf backend_startup.sh
+
+exec "$@"
