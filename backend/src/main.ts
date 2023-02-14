6624f39746b619504ae163f6c4d405b88a72aea4
Thibaut SIGUENZA <tsiguenz@e2r1p1.clusters.42paris.fr>
2023-02-14 18:37:58 +0100

    prettier and eslint config

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -3,11 +3,13 @@ import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	app.useGlobalPipes(new ValidationPipe({
-		whitelist: true,
-	}));
-	await app.listen(3000);
+  const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true
+    })
+  );
+  await app.listen(3000);
 }
 
 bootstrap();
