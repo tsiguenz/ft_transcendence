9219aea748f902bbeeeb998e90ea81719cbaa1d8
Crysicia <7459685+Crysicia@users.noreply.github.com>
2023-02-22 15:58:45 +0100

    Add multi env support

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -2,9 +2,19 @@ import { Module } from '@nestjs/common';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { PrismaModule } from './prisma/prisma.module';
+import { ConfigModule } from '@nestjs/config';
+
+const ENV = process.env.NODE_ENV;
 
 @Module({
-  imports: [AuthModule, UserModule, PrismaModule],
+  imports: [
+      ConfigModule.forRoot({
+        envFilePath: !ENV ? '.env' : `.env.${ENV}`,
+      }),
+      AuthModule,
+      UserModule,
+      PrismaModule
+    ],
   controllers: [],
   providers: [],
 })
