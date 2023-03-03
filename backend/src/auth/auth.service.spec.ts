ed8164cda41d2a98e2555d33bd727a3caa0c1ef0
Crysicia <7459685+Crysicia@users.noreply.github.com>
2023-03-03 19:35:14 +0100

    Add prisma mock

--- backend/src/auth/auth.service.spec.ts
+++ backend/src/auth/auth.service.spec.ts
@@ -1,17 +1,22 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { AuthService } from './auth.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { PrismaClient } from '@prisma/client';
 import { ConfigService } from '@nestjs/config';
 import { JwtService } from '@nestjs/jwt';
+import { mockDeep, DeepMockProxy } from 'jest-mock-extended';
 
 describe('AuthService', () => {
   let service: AuthService;
   let jwt: JwtService;
+  let prisma: DeepMockProxy<PrismaClient>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [PrismaService, ConfigService, AuthService, JwtService]
-    }).compile();
+    }).overrideProvider(PrismaService)
+      .useValue(mockDeep<PrismaClient>())
+      .compile();
 
     service = module.get<AuthService>(AuthService);
   });
