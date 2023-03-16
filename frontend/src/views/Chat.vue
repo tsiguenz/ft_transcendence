27449aca23e1ba78f851efdadac759be514763ec
Guillaume MORANGE <gmorange@e3r1p3.clusters.42paris.fr>
2023-03-16 15:34:25 +0100

    overflow looks good

--- frontend/src/views/Chat.vue
+++ frontend/src/views/Chat.vue
@@ -1,17 +1,23 @@
 <template>
   <h1>Chat</h1>
-  <v-sheet width="300" class="mx-auto">
-    <div v-for="message in messages">
-      From [{{ message.author }}]: {{ message.data }}
-    </div>
-    <v-form @submit.prevent>
-      <v-text-field
-        v-model="message"
-        label="Message"
-        v-on:keyup.enter="sendMessage"
-      ></v-text-field>
-    </v-form>
-  </v-sheet>
+  <v-card>
+    <v-card-text>
+      <v-list ref="chat" height="100" class="overflow-y-auto">
+        <div v-for="item in messages">
+          From [{{ item.author }}]: {{item.data }}
+        </div>
+      </v-list>
+    </v-card-text>
+    <v-card-actions>
+      <v-form @submit.prevent>
+        <v-text-field
+          v-model="message"
+          label="Message"
+          v-on:keyup.enter="sendMessage"
+        ></v-text-field>
+      </v-form>
+    </v-card-actions>
+  </v-card>
 </template>
 
 <script>
@@ -38,6 +44,9 @@ export default {
         message.author = 'Me';
       }
       this.messages.push(message);
+      this.$nextTick(() => {
+        this.$refs.chat.$el.scrollTop = this.$refs.chat.$el.scrollHeight;
+      });
     });
   },
   beforeUnmount() {
@@ -53,8 +62,12 @@ export default {
       // SocketioService.sendMessage("testChannel", this.message);
       this.messages.push({ author: 'Me', data: this.message });
       this.message = '';
+      console.table(this.messages);
+      this.$nextTick(() => {
+        this.$refs.chat.$el.scrollTop = this.$refs.chat.$el.scrollHeight;
+      });
     }
-  }
+  },
 };
 
 </script>
