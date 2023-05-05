dfc15531bcea13715f07878f96f9fbd9c03adaf6
tsiguenz <43640980+tsiguenz@users.noreply.github.com>
2023-05-05 13:58:43 +0200

    fix frontend wait backend is ready (#60)

--- docker/frontend_startup.sh
+++ docker/frontend_startup.sh
@@ -9,4 +9,15 @@ VITE_APP42_ID=$APP42_ID
 VITE_CALLBACK_URL=http://$HOST_IP:8080/42/callback
 EOF
 
+while true
+do
+    if curl backend:3000 > /dev/null 2>&1;
+    then
+        echo "Backend is up, starting frontend..."
+        break
+    fi
+    echo "Waiting for backend to start..."
+    sleep 10
+  done
+
 exec "$@"
