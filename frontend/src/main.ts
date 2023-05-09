831353ac830e3133f32b842f3bcc0d353ac1a140
tsiguenz <tsiguenz@student.42.fr>
2023-05-09 16:13:46 +0200

    axios interceptor now check if the jwt is expired before refresh the access_token

--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -12,6 +12,7 @@ import { VueCookieNext } from 'vue-cookie-next';
 import '../assets/style.scss';
 import axios from 'axios';
 import * as constants from './constants';
+import VueJwtDecode from 'vue-jwt-decode';
 
 const app = createApp(App);
 const pinia = createPinia();
@@ -41,30 +42,34 @@ axios.interceptors.response.use(
     return response;
   },
   async (error) => {
-    if (
-      error.response.status === 401 &&
-      error.config.url !== constants.API_URL + '/auth/refresh'
-    ) {
-      const refreshToken = VueCookieNext.getCookie('refresh_token');
-      try {
-        const response = await axios.post(
-          constants.API_URL + '/auth/refresh',
-          {},
-          { headers: { Authorization: `Bearer ${refreshToken}` } }
-        );
-        VueCookieNext.setCookie('jwt', response.data.access_token);
-        VueCookieNext.setCookie('refresh_token', response.data.refresh_token);
-        error.config.headers['Authorization'] =
-          'Bearer ' + response.data.access_token;
-        // fix socket disconnected when refresh page and jwt is expired
-        // reload page to restart the sockets with the new token
-        // its bad but idk how to do it better
-        window.location.reload();
-        return axios.request(error.config);
-      } catch (e) {
-        window.location.href = '/logout';
-      }
+    const isUnauthorized = error.response.status === 401;
+    const isRefreshUrl =
+      error.config.url === constants.API_URL + '/auth/refresh';
+    if (!isUnauthorized || isRefreshUrl)
+      return Promise.reject(error.response.data);
+    const jwt = VueCookieNext.getCookie('jwt');
+    const expirationTimestamp = VueJwtDecode.decode(jwt).exp;
+    const now = Math.floor(Date.now() / 1000);
+    if (now < expirationTimestamp) return Promise.reject(error);
+    // refresh the expired access_token
+    const refreshToken = VueCookieNext.getCookie('refresh_token');
+    try {
+      const response = await axios.post(
+        constants.API_URL + '/auth/refresh',
+        {},
+        { headers: { Authorization: `Bearer ${refreshToken}` } }
+      );
+      VueCookieNext.setCookie('jwt', response.data.access_token);
+      VueCookieNext.setCookie('refresh_token', response.data.refresh_token);
+      error.config.headers['Authorization'] =
+        'Bearer ' + response.data.access_token;
+      // fix socket disconnected when refresh page and jwt is expired
+      // reload page to restart the sockets with the new token
+      // its bad but idk how to do it better
+      window.location.reload();
+      return axios.request(error.config);
+    } catch (e) {
+      window.location.href = '/logout';
     }
-    return Promise.reject(error);
   }
 );
