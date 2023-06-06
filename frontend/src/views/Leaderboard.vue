9479295c9e9faceeb23a8ea73fd5a5a6a3f83f6e
tsiguenz <43640980+tsiguenz@users.noreply.github.com>
2023-06-06 11:50:07 +0200

    fix getFriend (#81)
    * fix getFriend
    
    * fix multiples call to getProfile
    
    * avoid multiple call to getFriend in isFriend component

--- frontend/src/views/Leaderboard.vue
+++ frontend/src/views/Leaderboard.vue
@@ -34,7 +34,11 @@
         <td class="hgt-td">{{ user.ladderPoints }}</td>
         <td class="hgt-td">{{ userStatus(user) }}</td>
         <td class="hgt-td">
-          <IsFriend :friendname="user.nickname"></IsFriend>
+          <IsFriend
+            v-if="!isMyProfile(user.nickname)"
+            :friendname="user.nickname"
+            :is-friend-at-begining="isFriend(user.nickname)"
+          ></IsFriend>
         </td>
       </tr>
     </tbody>
@@ -59,6 +63,7 @@ export default {
   inject: ['connectedUsersStore', 'sessionStore'],
   data() {
     return {
+      friends: [],
       users: [],
       connectedUsers: this.connectedUsersStore.connectedUsers
     };
@@ -83,8 +88,9 @@ export default {
       deep: true
     }
   },
-  async mounted() {
+  async created() {
     await this.getUsers();
+    await this.getFriends();
   },
   methods: {
     async getUsers() {
@@ -101,12 +107,12 @@ export default {
         this.$router.push('/logout');
       }
     },
-    async addFriend(friend) {
+    async getFriends() {
       try {
-        await axios.post(
-          constants.API_URL +
-            `/users/${this.sessionStore.nickname}/friends/${friend}`
+        const response = await axios.get(
+          constants.API_URL + `/users/${this.sessionStore.nickname}/friends`
         );
+        this.friends = response.data.map((friend) => friend.nickname);
       } catch (error) {
         swall({
           title: 'Error',
@@ -114,6 +120,7 @@ export default {
           icon: 'error',
           button: 'OK'
         });
+        this.$router.push('/logout');
       }
     },
     userStatus(user) {
@@ -130,6 +137,9 @@ export default {
     },
     getAvatarPath(user) {
       return constants.AVATARS_URL + user.avatarPath;
+    },
+    isFriend(nickname) {
+      return this.friends.includes(nickname);
     },
     isMyProfile(name) {
       return name === this.sessionStore.nickname;
