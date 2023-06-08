c06b3297870788098b88d72fa35ea818dea4d257
Siguenza92 <thibaut.siguenza@gmail.com>
2023-06-08 12:29:55 +0200

    format

--- backend/src/game/game.service.ts
+++ backend/src/game/game.service.ts
@@ -1,5 +1,4 @@
 import { Injectable } from '@nestjs/common';
-
 import { PrismaService } from '../prisma/prisma.service';
 import { Socket } from 'socket.io';
 import { AuthService } from '../auth/auth.service';
