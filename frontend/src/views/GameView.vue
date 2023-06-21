47d30411dab17e2eeb65c8ce72955b41be2a1e5a
tsiguenz <tsiguenz@student.42.fr>
2023-06-21 15:29:01 +0200

    fix customGame invitation

--- frontend/src/views/GameView.vue
+++ frontend/src/views/GameView.vue
@@ -5,7 +5,10 @@
     @change-status-to-in-menu="setStatusToInMenu()"
   />
 
-  <CustomGameMenu v-if="isInMenu()" />
+  <CustomGameMenu
+    v-if="isInMenu()"
+    @custom-room-created="subscribeCustomRoomCreated()"
+  />
 
   <WaitingGame v-if="isInQueue()" :is-ranked="isRanked" />
 
@@ -73,12 +76,6 @@ export default {
       this.socketioGame.send('connectToRoom');
       this.gameStatus = constants.GAME_STATUS.IN_QUEUE;
     },
-    createCustomRoom() {
-      this.isRanked = false;
-      this.subscribeGameLoop();
-      this.subscribeStartGame();
-      this.gameStatus = constants.GAME_STATUS.IN_QUEUE;
-    },
     joinCustomGame(gameId) {
       this.subscribeRoomNotFound();
       this.subscribeGameLoop();
@@ -125,6 +122,14 @@ export default {
         this.map = datas.map;
         this.score = datas.score;
         this.runGame();
+      });
+    },
+    subscribeCustomRoomCreated() {
+      this.socketioGame.subscribe('customRoomCreated', () => {
+        this.isRanked = false;
+        this.subscribeGameLoop();
+        this.subscribeStartGame();
+        this.gameStatus = constants.GAME_STATUS.IN_MENU;
       });
     },
     subscribeRoomNotFound() {
