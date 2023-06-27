fae7d39b6cd1ed2ccffc0c6e8878a012ac1e5395
tsiguenz <tsiguenz@student.42.fr>
2023-06-27 10:36:09 +0200

    choose game mode page style

--- frontend/src/components/ChooseGameMode.vue
+++ frontend/src/components/ChooseGameMode.vue
@@ -1,11 +1,17 @@
 <template>
   <v-container>
-    <v-btn class="pa-2 ma-2 log" @click="queueRanked"
-      >Search ranked match</v-btn
-    >
-    <v-btn class="pa-2 ma-2 log" @click="changeStatusToInMenu"
-      >Create custom game</v-btn
-    >
+    <v-row>
+      <v-col cols="6" align="right">
+        <v-icon class="icon-size" icon="mdi-flask-outline" />
+        <v-btn class="log" @click="queueRanked">Search ranked match</v-btn>
+      </v-col>
+      <v-col cols="6" align="left">
+        <v-icon class="icon-size" icon="mdi-trophy-outline" />
+        <v-btn class="log" @click="changeStatusToInMenu"
+          >Create custom game</v-btn
+        >
+      </v-col>
+    </v-row>
   </v-container>
 </template>
 
@@ -22,3 +28,17 @@ export default {
   }
 };
 </script>
+
+<style scoped>
+.icon-size {
+  font-size: 120px;
+  margin: 50px;
+}
+
+.div-choose-mode {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+}
+</style>
