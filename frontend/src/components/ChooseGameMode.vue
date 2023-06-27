38e5030176f422390c9857fcbe3cbc01cc0265ed
tsiguenz <tsiguenz@student.42.fr>
2023-06-27 12:47:41 +0200

    oups bad order

--- frontend/src/components/ChooseGameMode.vue
+++ frontend/src/components/ChooseGameMode.vue
@@ -2,11 +2,11 @@
   <v-container>
     <v-row>
       <v-col cols="6" align="right">
-        <v-icon class="icon-size" icon="mdi-flask-outline" />
+        <v-icon class="icon-size" icon="mdi-trophy-outline" />
         <v-btn class="log" @click="queueRanked">Search ranked match</v-btn>
       </v-col>
       <v-col cols="6" align="left">
-        <v-icon class="icon-size" icon="mdi-trophy-outline" />
+        <v-icon class="icon-size" icon="mdi-flask-outline" />
         <v-btn class="log" @click="changeStatusToInMenu"
           >Create custom game</v-btn
         >
